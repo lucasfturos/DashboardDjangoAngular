fc8e4ef79147c23126d32804932977b58bc10789
lucasfturos <lucasfturos@gmail.com>
2022-12-23 18:34:25 -0300

    Organização das pastas

--- server/admin/asgi.py
+++ server/admin/asgi.py
@@ -1,5 +1,5 @@
 """
-ASGI config for APIProdutosDjango project.
+ASGI config for DashboardDjangoAngular project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'APIProdutosDjango.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'DashboardDjangoAngular.settings')
 
 application = get_asgi_application()
