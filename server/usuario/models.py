d6766fdf657a43565b6951b3d8235fe5e011606e
lucasfturos <lucasfturos@gmail.com>
2022-12-30 09:12:25 -0300

    Atualização do models do funcionario e cliente

--- server/usuario/models.py
+++ server/usuario/models.py
@@ -2,12 +2,17 @@ from django.db import models
 
 class Funcionario(models.Model):
     id = models.BigAutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')
-    nome_usuario = models.CharField(default='',max_length=30)
-    email_usuario = models.CharField(max_length=30)
-    username = models.CharField(default='',max_length=20)
-    password = models.CharField(default='',max_length=20)
+    nome_completo_usuario = models.CharField(default='',max_length=60)
+    email_usuario = models.CharField(default='',max_length=30)
+    apelido_usuario = models.CharField(default='',max_length=20)
+    senha_usuario = models.CharField(default='',max_length=20)
+    celular_usuario = models.CharField(default='',max_length=12)
+    cpf_cnpj_usuario = models.CharField(default='',max_length=20)
+    cep_usuario = models.CharField(default='', max_length=10)
+    endereco_usuario = models.CharField(default='', max_length=255)
+    complemento_usuario = models.CharField(default='',max_length=255)
+    token_usuario = models.CharField(default='',max_length=255)
     id_niveis=models.ForeignKey('NiveisUsuario', max_length=10, on_delete=models.CASCADE)
-    
 
     def __str__(self):
         return 'O Usuário {} é {}'.format(self.nome_usuario, self.id_niveis) 
@@ -18,10 +23,14 @@ class Cliente(models.Model):
     id = models.BigAutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')
     nome_cliente = models.CharField(max_length=30)
     email_cliente = models.CharField(max_length=30)
-    id_niveis=models.ForeignKey('NiveisUsuario', max_length=10, on_delete=models.CASCADE)
+    celular_cliente = models.CharField(default='',max_length=12)
+    cpf_cnpj_cliente = models.CharField(default='',max_length=20)
+    cep_cliente = models.CharField(default='', max_length=10)
+    endereco_cliente = models.CharField(default='', max_length=255)
+    complemento_cliente = models.CharField(default='',max_length=255)
 
     def __str__(self):
-        return 'O Usuário {} é {}'.format(self.nome_cliente, self.id_niveis) 
+        return 'O Cliente {} portador do E-mail {}'.format(self.nome_cliente, self.email_cliente) 
 
 class NiveisUsuario(models.Model):
     id = models.BigAutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')
